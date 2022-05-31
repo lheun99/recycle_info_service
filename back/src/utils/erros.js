e0b3490dbdae75d988c83959bbc13bda5d25e93b
YunSeongJun <movomo@gmail.com>
2022-05-31 13:39:17 +0900

    chore: remove legacy request error class

--- back/src/utils/erros.js
+++ back/src/utils/erros.js
@@ -3,39 +3,6 @@ import {
     STATUS_500_INTERNALSERVERERROR,
   } from "./status.js";
 
-  /** Request Error Class that blames (mostly) bad request kind.
-   *
-   * ## Constructor
-   * @constructor
-   * @param {{status: number, payload: any}} payload
-   * @param {any[]} params
-   * ```js
-   *  {
-   *      status: number = 400,
-   *      payload: any = { success: false }
-   *  }, ...params
-   * ```
-   *  - `status`: This is sent to errorMiddleware and then sent to the browser.
-   *  - `payload` holds res data to send it even in the brink of erroring out.
-   *  - `params`: Anything that you would otherwise pass to an Error.
-   */
-  class ObsoleteRequestError extends Error {
-    constructor(
-      { status = STATUS_400_BADREQUEST, payload = { success: false } },
-      ...params
-    ) {
-      super(...params);
-
-      if (Error.captureStackTrace) {
-        Error.captureStackTrace(this, ObsoleteRequestError);
-      }
-
-      this.name = this.constructor.name;
-      this.status = status;
-      this.payload = payload;
-    }
-  }
-
   /** 기본 `Error` 타입에 더 구체적인 정보를 담을 수 있도록 확장한 범용 에러입니다. */
   class AppError extends Error {
     static status = STATUS_500_INTERNALSERVERERROR;
@@ -105,4 +72,4 @@ import {
     }
   }
 
-  export { ObsoleteRequestError as RequestError, AppError };
+  export { RequestError, AppError };
