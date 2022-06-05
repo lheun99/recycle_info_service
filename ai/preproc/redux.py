b6d73a97edcfd6873a6bd63ceabea87d5a4314f4
YunSeongJun <movomo@gmail.com>
2022-06-05 16:33:32 +0900

    code: overhaul args

--- ai/preproc/redux.py
+++ ai/preproc/redux.py
@@ -24,68 +24,74 @@ def _getargs() -> argparse.Namespace:
         ),
     )
     parser.add_argument(
-        'image_top',
-        required=True,
+        '--image-src',
+        '-ims',
         help=(
             '이미지 파일이 있는 최상위 경로입니다.'
             ' 이 경로로부터 상대 경로를 산출합니다.'
         ),
     )
     parser.add_argument(
-        'json_top',
-        required=True,
-        help=(
-            '애노테이션 json 파일들이 있는 최상위 경로입니다.'
-            ' json 포맷만 알아서 골라냅니다.'
-            ' 디렉터리 구조가 이미지와 같아야 합니다.'
-        ),
+        '--image-dst',
+        '-imd',
+        help='출력 이미지를 저장할 경로입니다. 디렉터리 구조를 보존합니다.',
     )
     parser.add_argument(
-        'dest',
-        required=True,
+        '--image-output-dimension',
+        '-dim',
         help=(
-            '출력 내용을 저장할 경로입니다.'
-            ' 애노테이션 파일은 append 모드로 적습니다.'
-            ' 이미지 파일 상대 경로 등 디렉터리 구조는 보존합니다.'
+            '이미지 크기를 조정할 목표 값을 WxH 형식으로 지정합니다.'
+            '기본값은 640x640입니다.'
         ),
+        type=whtuple,
+        default=(640, 640),
     )
 
     parser.add_argument(
-        '--format',
-        '-f',
+        '--label-src',
+        '-lbs',
         help=(
-            '애노테이션 포맷입니다.'
-            ' 기본값은 "{image_path}, {xmin}, {ymin}, {xmax}, {ymax}, {classid}"'
-            '입니다.'
+            'aihub 애노테이션 json 파일들이 있는 최상위 경로입니다.'
+            ' json 포맷만 알아서 골라냅니다.'
+            ' 이미지를 처리할 때는 디렉터리 구조가 이미지와 같아야 합니다.'
         ),
-        default='{image_path},{xmin},{ymin},{xmax},{ymax},{classid}',
     )
     parser.add_argument(
-        '--minimum-dimension',
-        '--min-dim',
-        '-md',
+        '--label-dst',
+        '-lbd',
         help=(
-            '이미지의 가로, 세로 중 작은 쪽이 이 값이 되도록 리사이즈합니다.'
-            '기본값은 512입니다.'
+            '출력 내용을 저장할 경로입니다.'
+            ' 애노테이션 파일은 append 모드로 적습니다.'
+            ' 디렉터리 구조는 보존합니다.'
         ),
-        type=int,
-        default=512,
     )
     parser.add_argument(
-        '--threads',
-        '-nt',
-        help='작업 스레드의 수입니다. 0(기본값)이면 자동으로 맞춥니다.',
-        type=int,
-        default=0,
+        '--label-output-type',
+        '-lbf',
+        help='애노테이션 포맷입니다. yolov3, yolov5 중 하나입니다.',
+        choices=['yolov3', 'yolov5'],
+    )
+
+    parser.add_argument(
+        '--parellelize',
+        '-p',
+        help=(
+            '병렬화 방식입니다.'
+            "'multithreading', 'mt', 'multiprocessing', 'mp' 중의 하나입니다."
+            '스레드/프로세스의 수는 자동으로 결정합니다.'
+            '지정하지 않으면 병렬화를 시도하지 않습니다.'
+        ),
+        choices=['multithreading', 'mt', 'multiprocessing', 'mp'],
+        default=None,
     )
 
     args = parser.parse_args()
     # return args, args.__dict__.copy()
-    top = path.normcase(path.normpath(args.image_top))
-    args.json_top = path.normcase(path.normpath(args.json_top))
-    args.dest = path.normcase(path.normpath(args.dest))
-    if args.threads == 0:
-        args.threads = None
+    for arg in ['image_src', 'image_dst', 'label_src', 'label_dst']:
+        pathname = args.__dict__[arg]
+        args.__dict__[arg] = path.abspath(
+            path.normpath(path.normcase(pathname))
+        )
 
     return args
 
@@ -98,7 +104,7 @@ def cli():
         f'{args.threads if args.threads is not None else executor._max_workers}'
         '개 사용합니다.')
 
-    for stem, branches, leaves in os.walk(args.json_top):
+    for stem, branches, leaves in os.walk(args.label_src):
         for leaf in leaves:
             if path.splitext(leaf)[1].lower() == 'json':
                 pass
