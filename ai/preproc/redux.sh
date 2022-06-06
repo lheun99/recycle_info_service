7fb3453bc43ee3e86254cfc9894912d9f6e060ab
YunSeongJun <movomo@gmail.com>
2022-06-07 00:24:38 +0900

    test: done convert label to yolov5

--- ai/preproc/redux.sh
+++ ai/preproc/redux.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env sh
 TEST=1
-python3=../../.venv/Scripts/python
+# python3=../../.venv/Scripts/python
 
 
 if [ $TEST -eq 0 ]; then
@@ -15,14 +15,21 @@ fi
 TESTDIR=/d/movomo/Downloads/test
 
 # Test process labels to pickle
-# $python3 ./redux.py \
+# python ./redux.py \
 #     --label-src=$TESTDIR/labels \
 #     --label-dst=$TESTDIR/pickle/test.pickle \
 #     --label-output-type=pickle
 
 # Test process labels to yolov3
-# $python3 ./redux.py \
+# python ./redux.py \
 #     --image-output-dimension=640x640 \
 #     --label-src=$TESTDIR/labels \
 #     --label-dst=$TESTDIR/yolov3/test.text \
 #     --label-output-type=yolov3
+
+# Test process labels to yolov5
+# python ./redux.py \
+#     --image-output-dimension=640x640 \
+#     --label-src=$TESTDIR/labels \
+#     --label-dst=$TESTDIR/yolov5 \
+#     --label-output-type=yolov5
