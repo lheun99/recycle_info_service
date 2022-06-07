6436e63140bb97bec6e07fc078b5ede8f60f5cc0
YunSeongJun <movomo@gmail.com>
2022-06-07 14:57:31 +0900

    chore: change download path to nas

--- ai/preproc/redux.sh
+++ ai/preproc/redux.sh
@@ -2,7 +2,8 @@
 TEST=0
 # python3=../../.venv/Scripts/python
 
-DATA_IN='/d/movomo/Downloads/생활 폐기물 이미지'
+# DATA_IN='/d/movomo/Downloads/생활 폐기물 이미지'
+DATA_IN='/s/public.downloads/생활 폐기물 이미지'
 DATA_OUT='/d/OneDrive/.attach/cyberdyne/dataset'
 
 # 이미지 저장을 위해 라벨은 그대로 두고 이미지만 640x640 크기로 조정합니다.
