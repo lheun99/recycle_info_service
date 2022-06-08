27d9d1b38fcd7a1f49f604a27cfeec21d84ee568
DOHEE <dohee212@naver.com>
2022-06-08 14:11:44 +0900

    [Feat] userRouter 파일 생성하고 유효성 검사 후 반환하는 간단한 router 생성

---
+++ back/src/routes/userRouter.js
@@ -0,0 +1,29 @@
+const userRouter = require("express").Router();
+// const UserService = require("../services/userService");
+const { body, validationResult } = require("express-validator");
+
+userRouter.post(
+    "/register",
+    body("email").isEmail().withMessage("이메일 형식이 올바르지 않습니다."),
+    body("password")
+        .isLength({ min: 8, max: 16 })
+        .withMessage("8 ~ 16자리 비밀번호를 입력해주세요"),
+    (req, res, next) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                throw new Error(errors.errors[0].msg);
+            }
+
+            const { nickname, email, password } = req.body;
+
+            const newUser = { nickname, email, password };
+
+            res.status(201).json(newUser);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+module.exports = userRouter;
