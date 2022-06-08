76bbec17cd1d0ef5c66da0b57cadda9763167e80
DOHEE <dohee212@naver.com>
2022-06-08 20:18:14 +0900

    [Feat] user schema에 null 불가 조항 작성

--- back/src/models/schemas/user.js
+++ back/src/models/schemas/user.js
@@ -2,12 +2,15 @@ module.exports = (sequelize, Sequelize) => {
     const User = sequelize.define("user", {
         nickname: {
             type: Sequelize.STRING,
+            allowNull: false,
         },
         email: {
             type: Sequelize.STRING,
+            allowNull: false,
         },
         password: {
             type: Sequelize.TEXT,
+            allowNull: false,
         },
         totalPoint: {
             type: Sequelize.BIGINT,
