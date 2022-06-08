0e8d7cef3e5fa6c89fd65c3a78d72b846454cfaa
DOHEE <dohee212@naver.com>
2022-06-08 15:09:11 +0900

    [Feat] email로 가입이력 확인하는 코드 작성'

--- back/src/services/userService.js
+++ back/src/services/userService.js
@@ -2,6 +2,14 @@ const User = require("../models/funcs/User");
 
 const userService = {
     addUser: async ({ nickname, email, password }) => {
+        const user = await User.findByEmail({ email });
+
+        if (user) {
+            throw new Error(
+                "이 이메일은 현재 사용중입니다. 다른 이메일을 입력해 주세요."
+            );
+        }
+
         const newUser = { nickname, email, password };
         console.log(newUser);
         const createdNewUser = await User.create({ newUser });
