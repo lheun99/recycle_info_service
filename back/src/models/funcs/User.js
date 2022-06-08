b232e772518c90065a5ce112c91c2293031e80a1
DOHEE <dohee212@naver.com>
2022-06-08 14:49:00 +0900

    [Feat] 새로운 회원정보를 db에 저장하는 코드 작성

--- back/src/models/funcs/User.js
+++ back/src/models/funcs/User.js
@@ -2,8 +2,13 @@ const db = require("../index.js");
 const userModel = db.user;
 
 const User = {
-    create: () => {
-        console.log("say hello!");
+    create: async ({ newUser }) => {
+        console.log("잘 넘어오니?", newUser);
+        const createdNewUser = await userModel.create(newUser, {
+            fields: ["nickname", "email", "password"],
+        });
+
+        return { message: "success", data: createdNewUser };
     },
 };
 
