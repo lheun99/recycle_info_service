f91585b74000dd61d290b7a93a0d05f3aa1d1fa3
DOHEE <dohee212@naver.com>
2022-06-09 15:44:05 +0900

    [Feat] 시간을 조건으로 포인트 조회하는 모델 코드 작성

--- back/src/models/funcs/Point.js
+++ back/src/models/funcs/Point.js
@@ -1,5 +1,6 @@
 const db = require("../index.js");
 const pointModel = db.point;
+const Op = db.Sequelize.Op;
 
 const Point = {
     create: async ({ newPoint }) => {
@@ -8,6 +9,13 @@ const Point = {
         });
 
         return createdNewPoint;
+    },
+
+    findByFilter: async ({ userId, route, today }) => {
+        const point = await pointModel.findOne({
+            where: { userId, route, createdAt: { [Op.gt]: today } },
+        });
+        return point;
     },
 
     findAllById: async ({ userId }) => {
