86b1a31a54f5803b4cc73607557c9c603ccfc188
DOHEE <dohee212@naver.com>
2022-06-09 18:11:15 +0900

    [Feat] setUtil에 리스트를 섞어주는 shuffle 추가

--- back/src/utils/setUtil.js
+++ back/src/utils/setUtil.js
@@ -8,6 +8,11 @@ const setUtil = {
         });
         return toUpdate;
     },
+
+    shuffle: (datalist) => {
+        const datalist = datalist.sort(() => Math.random() - 0.5);
+        return datalist;
+    },
 };
 
 module.exports = setUtil;
