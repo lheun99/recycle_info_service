13794a46a49b86b9719c669b1d01ca61d64a9775
DOHEE <dohee212@naver.com>
2022-06-09 15:51:14 +0900

    [Feat] 반환 형식 수정

--- back/src/services/pointService.js
+++ back/src/services/pointService.js
@@ -4,19 +4,25 @@ const pointService = {
     addPoint: async ({ userId, route, point }) => {
         const newPoint = { userId, route, point };
         const createdNewPoint = await Point.create({ newPoint });
-        return createdNewPoint;
+        return { message: "success", data: createdNewPoint };
     },
 
     getPoint: async ({ userId, route }) => {
         // 2022-06-08 17:17:21.831 +0900
         const today = "2022-06-08T15:00:00.000Z";
-        const point = await Point.findByFilter({ userId, route, today });
-        return point;
+        let point = await Point.findByFilter({ userId, route, today });
+
+        if (!point) {
+            point = { point: false };
+        } else {
+            point = { point: point.point };
+        }
+        return { message: "success", data: point };
     },
 
     getPoints: async ({ userId }) => {
         const points = await Point.findAllById({ userId });
-        return points;
+        return { message: "success", data: points };
     },
 };
 
