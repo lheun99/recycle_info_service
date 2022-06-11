23a9e25a7c73f56c5fb9511f7b2cd4d427ba1064
YunSeongJun <movomo@gmail.com>
2022-06-11 20:21:23 +0900

    code: done test_image()

--- ai/preproc/test-image.py
+++ ai/preproc/test-image.py
@@ -9,7 +9,14 @@ from tqdm import tqdm
 
 
 def test_image(pathname: str) -> bool:
-    pass
+    '''이미지가 제대로 된 파일인지 검사하고 정상이면 참, 아니면 거짓을 반환합니다.'''
+    try:
+        with Image.open(pathname) as image:
+            image.verify()
+    except Exception:
+        return False
+    else:
+        return True
 
 
 def cli():
@@ -20,7 +27,6 @@ def cli():
         'src',
         help='원본 이미지가 들어있는 최상위 디렉터리 경로입니다.',
     )
-    parser.add_argument()
 
     args = parser.parse_args()
 
