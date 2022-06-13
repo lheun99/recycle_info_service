011040a8718edbcaadfff63b9e378d9e91102ed5
jinah park <tbrk8585@naver.com>
2022-06-13 16:41:24 +0900

    feat: userProfile edit 창 생성

--- front/components/MyPage/UserProfile.tsx
+++ front/components/MyPage/UserProfile.tsx
@@ -1,10 +1,19 @@
-import React from "react";
-import Avatar from "@mui/material/Avatar";
+import React, { useState } from "react";
 import Image from "next/image";
 import setting from "../../public/setting.png";
 import myPageStyles from "../../styles/myPage.module.css";
+import {
+    Avatar,
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogActions,
+} from "@mui/material";
+import ImageUpload from "../recycling/ImageUpload";
 
 const UserProfile = () => {
+    const [isEditing, setIsEditing] = useState(false);
+
     return (
         <div>
             {" "}
@@ -20,10 +29,34 @@ const UserProfile = () => {
                 <h5>jaPark@naver.com</h5>
             </div>
             <div>
-                <button type="button" className={myPageStyles.profile_button}>
+                <button
+                    type="button"
+                    className={myPageStyles.profile_button}
+                    onClick={() => setIsEditing(!isEditing)}
+                >
                     <Image src={setting} alt="setting button" />
                 </button>
             </div>
+            {isEditing && (
+                <Dialog
+                    open={isEditing}
+                    onClose={() => setIsEditing((cur) => !cur)}
+                >
+                    <DialogTitle>프로필 편집</DialogTitle>
+                    <DialogContent>
+                        <Avatar
+                            alt="user profile"
+                            src="logo.png"
+                            sx={{ width: "200px", height: "200px" }}
+                        />
+                        <ImageUpload />
+                    </DialogContent>
+                    <DialogActions>
+                        <button>변경하기</button>
+                        <button>취소하기</button>
+                    </DialogActions>
+                </Dialog>
+            )}
         </div>
     );
 };
