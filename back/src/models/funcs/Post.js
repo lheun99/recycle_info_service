dc21fca0e792a6ea6614fdb20bf17a1d85c9991b
Lee Ha-eun <lheun99@gmail.com>
2022-06-14 22:46:18 +0900

    feat : post MVP 관련 기능 추가 및 수정

--- back/src/models/funcs/Post.js
+++ back/src/models/funcs/Post.js
@@ -1,25 +1,35 @@
 const db = require("../index.js");
 const postModel = db.post;
 const Sequelize = db.Sequelize;
+const sequelize = db.sequelize;
 
 const Post = {
   create: async ({ newPost }) => {
     const createdNewPost = await postModel.create(newPost, {
-      fields: ["title", "post_img", "content"],
+      fields: ["user_id", "title", "post_img", "content"],
     });
 
     return createdNewPost;
   },
 
+  findAllPost: async () => {
+    const postlist = await sequelize.query(
+      `SELECT posts.title, posts."createdAt", users.nickname, posts.post_img
+      FROM posts 
+      INNER JOIN users 
+      ON posts.user_id=users.user_id 
+      ORDER BY posts."createdAt"`
+    );
+    return postlist[0];
+  },
+  findPostByUserId: async ({ user_id }) => {
+    const posts = await postModel.findAll({ where: { user_id } });
+    return posts;
+  },
   findPostById: async ({ post_id }) => {
     const post = await postModel.findOne({ where: { post_id } });
     return post;
   },
-  // findAllPost: async () => {
-  //   const postlist = await postModel.findAll({});
-
-  //   return postlist;
-  // },
 
   update: async ({ post_id, toUpdate }) => {
     const updatedPost = await postModel.update(toUpdate, {
