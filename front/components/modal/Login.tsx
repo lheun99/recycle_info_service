8f7a6da8090e4bc2475187319a23d5f5370877c1
aldrn <aldrn29@gmail.com>
2022-06-15 22:05:14 +0900

    feat: 회원가입 화면 구현

--- front/components/modal/Login.tsx
+++ front/components/modal/Login.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
-import { Modal, Button, TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import LoginStyles from "../../styles/Login.module.css";
 import Image from "next/image";
 import Logo from "../../public/images/logo.png";
 
-function Login({ open, handleClose }) {
+function Login({ open, handleClose, setRegister }) {
     const [email, setEmail] = useState<String>("");
     const [password, setPassword] = useState<String>("");
 
-    const validateEmail = (email) => {
+    const validateEmail = (email: String) => {
         return email
             .toLowerCase()
             .match(
@@ -25,86 +25,81 @@ function Login({ open, handleClose }) {
     }
 
     return (
-        open && (
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <div className={LoginStyles.login_wrapper}>
-                    <div style={{ textAlign: "right" }}>
-                        <Button
-                            variant="text"
-                            className={LoginStyles.close_button}
-                            onClick={handleClose}
-                        >
-                            x
-                        </Button>
-                    </div>
-                    <div className={LoginStyles.logo}>
-                        <Image
-                            src={Logo}
-                            alt="logo"
-                            width={40}
-                            height={40}
-                        />
-                    </div>
-                    <div className={LoginStyles.login_form}>
-                        <TextField
-                            type="email"
-                            style={{
-                                width: "380px",
-                                height: "40px",
-                            }}
-                            label="E-MAIL"
-                            size="small"
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                        <TextField
-                            type="password"
-                            style={{
-                                width: "380px",
-                                height: "40px",
-                            }}
-                            label="PASSWORD"
-                            size="small"
-                            onChange={(e) =>
-                                setPassword(e.target.value)
-                            }
-                            helperText={
-                                isPasswordValid
-                                    ? ""
-                                    : "비밀번호는 4글자 이상입니다."
-                            }
-                        />
-                        <Button
-                            className={LoginStyles.login_button}
-                            onClick={handleSubmit}
-                            disabled={!isFormValid}
-                        >
-                            LOGIN
-                        </Button>
-                    </div>
-                    <div className={LoginStyles.or}>or</div>
-                        <div className={LoginStyles.login_bottom}>
-                            <Button
-                                variant="text"
-                                className=""
-                            >
-                                비밀번호 찾기
-                            </Button>
-                            <span>|</span>
-                            <Button
-                                variant="text"
-                            >
-                                회원가입  
-                            </Button>
-                    </div>
-                </div>
-            </Modal>
-        )
-    );
+        <div className={LoginStyles.login_wrapper}>
+            <div style={{ textAlign: "right" }}>
+                <Button
+                    variant="text"
+                    className={LoginStyles.close_button}
+                    onClick={handleClose}
+                >
+                    x
+                </Button>
+            </div>
+            <div className={LoginStyles.logo}>
+                <Image
+                    src={Logo}
+                    alt="logo"
+                    width={40}
+                    height={40}
+                />
+            </div>
+            <div className={LoginStyles.login_form}>
+                <TextField
+                    type="email"
+                    style={{
+                        width: "380px",
+                        height: "40px",
+                    }}
+                    label="E-MAIL"
+                    size="small"
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <TextField
+                    type="password"
+                    style={{
+                        width: "380px",
+                        height: "40px",
+                    }}
+                    label="PASSWORD"
+                    size="small"
+                    onChange={(e) =>
+                        setPassword(e.target.value)
+                    }
+                    helperText={
+                        isPasswordValid
+                            ? ""
+                            : "비밀번호는 4글자 이상입니다."
+                    }
+                />
+                <Button
+                    className={LoginStyles.login_button}
+                    onClick={handleSubmit}
+                    disabled={!isFormValid}
+                >
+                    Sign in
+                </Button>
+            </div>
+            <div className={LoginStyles.or}>or</div>
+            <div className={LoginStyles.login_bottom}>
+                <Button
+                    variant="text"
+                    className=""
+                >
+                    비밀번호 찾기
+                </Button>
+                <span>|</span>
+                <Button
+                    variant="text"
+                    onClick={() => {
+                        handleClose
+                        setRegister(true)
+                    }}
+                >
+                    회원가입  
+                </Button>
+            </div>
+        </div>
+    )
 }
 
 export default Login;
--- front/components/modal/Register.tsx
+++ front/components/modal/Register.tsx
@@ -1,10 +1,112 @@
-import React from "react";
+import React, { useState } from "react";
+import { Modal, Button, TextField } from "@mui/material";
+import LoginStyles from "../../styles/Login.module.css";
+import Image from "next/image";
+import Forest from "../../public/images/forest.jpeg";
 
-const Register = () => {
+const Register = ({ open, handleClose, setRegister }) => {
+    const [email, setEmail] = useState<String>("");
+    const [password, setPassword] = useState<String>("");
+    const [confirmPassword, setConfirmPassword] = useState<String>("");
+
+    const validateEmail = (email: String) => {
+        return email
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            )
+    }
+
+    const isEmailValid = validateEmail(email)
+    const isPasswordValid = password.length >= 4
+    const isPasswordSame = password === confirmPassword
+    const isFormValid = isEmailValid && isPasswordValid && isPasswordSame
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+    };
 
     return (
-        <div>
-
+        <div className={LoginStyles.register}>
+            <section className={LoginStyles.register_image}>
+                <Image
+                    src={Forest}
+                    alt="forest"
+                    width={430}
+                    height={600}
+                    objectFit="cover"
+                />
+            </section>
+            <section className={LoginStyles.register_wrapper}>
+                <div style={{ textAlign: "right" }}>
+                    <Button
+                        variant="text"
+                        className={LoginStyles.close_button}
+                        onClick={() => {
+                            setRegister(false)
+                            handleClose()
+                        }}
+                    >
+                        x
+                    </Button>
+                </div>
+                <div className={LoginStyles.register_title}>Create Account</div>
+                <div className={LoginStyles.register_form}>
+                    <TextField
+                        type="email"
+                        style={{
+                            width: "380px",
+                            height: "40px",
+                        }}
+                        label="E-MAIL"
+                        size="small"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <TextField
+                        type="password"
+                        style={{
+                            width: "380px",
+                            height: "40px",
+                        }}
+                        label="PASSWORD"
+                        size="small"
+                        onChange={(e) => setPassword(e.target.value)}
+                        helperText={
+                            isPasswordValid
+                                ? ""
+                                : "비밀번호는 4글자 이상입니다."
+                        }
+                    />
+                    <TextField
+                        type="password"
+                        style={{
+                            width: "380px",
+                            height: "40px",
+                        }}
+                        label="CONFIRM PASSWORD"
+                        size="small"
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        helperText={
+                            isPasswordSame ? "비밀번호는 4글자 이상입니다." : "비밀번호가 일치하지 않습니다."
+                        }
+                    />
+                    <Button
+                        variant="text"
+                        type="submit"
+                        className={LoginStyles.login_button}
+                        onClick={() => {
+                            handleSubmit
+                            setRegister(false)
+                        }}
+                        disabled={!isFormValid}
+                    >
+                        Sign up
+                    </Button>
+                </div>
+                <div className={LoginStyles.or}>or</div>
+            </section>
         </div>
     )
 } 
+
+export default Register;
