8217aaac005171c565c7a08c51ee191c535b5f9f
YunSeongJun <movomo@gmail.com>
2022-06-16 17:57:38 +0900

    code: declare Box classmethods

--- ai/preproc/label.py
+++ ai/preproc/label.py
@@ -4,7 +4,8 @@ from typing import (
     NamedTuple,
     NewType,
     Optional,
-    Tuple
+    Tuple,
+    Union,
 )
 
 
@@ -25,10 +26,40 @@ class Rect(NamedTuple):
         return (*self.min, *self.max)
 
 
+class Resolution(NamedTuple):
+    width: Number
+    height: Number
+
+    @property.getter
+    def w(self):
+        return self.width
+
+    @property.getter
+    def h(self):
+        return self.height
+
+
 class Box(object):
     '''객체 탐지 시 바운딩 박스 한개입니다. 좌표를 분율로 저장합니다.'''
     classid: int
     center: Coord
+    res: Resolution
+    rect: Rect
 
-    def __init__(self) -> None:
+    def __init__(
+        self, classid: int, center: Coord, res: Resolution, rect: Rect
+    ) -> None:
+        self.classid = classid
+        self.center = center
+        self.res = res
+        self.rect = rect
+
+    @classmethod
+    def from_centerbox(
+        cls, center: Tuple[Number, Number], res: Tuple[Number, Number]
+    ):
         pass
+
+    @classmethod
+    def from_rect(cls, rect: Tuple[Number, Number, Number, Number]):
+        raise NotImplementedError('Not needed yet, so not implemented yet')
