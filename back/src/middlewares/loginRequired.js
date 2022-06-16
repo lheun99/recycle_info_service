82e0d1e58e93c962cf3d6c815c94dd04c1097da6
DOHEE <dohee212@naver.com>
2022-06-16 16:22:06 +0900

    [Fix] 오류 해결

--- back/src/middlewares/loginRequired.js
+++ back/src/middlewares/loginRequired.js
@@ -15,24 +15,23 @@ const loginRequired = (req, res, next) => {
         res.status(400).json("로그인한 유저만 사용할 수 있는 서비스입니다.");
         return;
     }
-    res.status(400).json("로그인한 유저만 사용할 수 있는 서비스입니다.");
-    return;
-  }
 
-  // 해당 token 이 정상적인 token인지 확인 -> 토큰에 담긴 user_id 정보 추출
-  try {
-    if (!process.env.JWT_SECRET_KEY) {
-      throw new Error("JWT_SECRET_KEY가 존재하지 않습니다.");
+    // 해당 token 이 정상적인 token인지 확인 -> 토큰에 담긴 user_id 정보 추출
+    try {
+        if (!process.env.JWT_SECRET_KEY) {
+            throw new Error("JWT_SECRET_KEY가 존재하지 않습니다.");
+        }
+        const secretKey = process.env.JWT_SECRET_KEY;
+        const jwtDecoded = jwt.verify(userToken, secretKey);
+        const userId = jwtDecoded.userId;
+        req.currentUserId = userId;
+        next();
+    } catch (error) {
+        res.status(400).json(
+            "정상적인 토큰이 아닙니다. 다시 한 번 확인해 주세요."
+        );
+        return;
     }
-    const secretKey = process.env.JWT_SECRET_KEY;
-    const jwtDecoded = jwt.verify(userToken, secretKey);
-    const userId = jwtDecoded.userId;
-    req.currentUserId = userId;
-    next();
-  } catch (error) {
-    res.status(400).json("정상적인 토큰이 아닙니다. 다시 한 번 확인해 주세요.");
-    return;
-  }
 };
 
 module.exports = loginRequired;
