811f36eefdefa81a560cf1a9c72b9fe27a6dcf4b
YunSeongJun <movomo@gmail.com>
2022-06-16 23:18:36 +0900

    code: add exp-oneepoch.sh

---
+++ ai/train/exp-oneepoch.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env sh
+
+# epoch 하나에 걸리는 시간을 측정합니다.
+
+YOLOREPO="$HOME/yolov5"
+DST="$HOME/train"
+
+python "$YOLOREPO/train.py" \
+    --weights '' \
+    --cfg "$YOLOREPO/models/yolov5m.yaml" \
+    --data "$REPO/ai/train/data.yaml" \
+    --epochs 3 \
+    --batch-size -1 \
+    --project "$DST/exp.oneepoch" \
