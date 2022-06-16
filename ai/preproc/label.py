9499fed71fbb841e0cf8f5d6c5edac9e54495960
YunSeongJun <movomo@gmail.com>
2022-06-16 18:13:26 +0900

    code: add Box classmethods

--- ai/preproc/label.py
+++ ai/preproc/label.py
@@ -9,12 +9,12 @@ from typing import (
 )
 
 
-Number = NewType('Number', [int | float])
+# Number = NewType('Number', [int | float])
 
 
 class Coord(NamedTuple):
-    x: Number
-    y: Number
+    x: float
+    y: float
 
 
 class Rect(NamedTuple):
@@ -26,9 +26,9 @@ class Rect(NamedTuple):
         return (*self.min, *self.max)
 
 
-class Resolution(NamedTuple):
-    width: Number
-    height: Number
+class ImageSize(NamedTuple):
+    width: float
+    height: float
 
     @property.getter
     def w(self):
@@ -40,26 +40,38 @@ class Resolution(NamedTuple):
 
 
 class Box(object):
-    '''객체 탐지 시 바운딩 박스 한개입니다. 좌표를 분율로 저장합니다.'''
+    '''객체 탐지 시 바운딩 박스 한개입니다. 좌표는 모두 분율입니다.'''
     classid: int
     center: Coord
-    res: Resolution
+    size: ImageSize
     rect: Rect
 
     def __init__(
-        self, classid: int, center: Coord, res: Resolution, rect: Rect
+        self, classid: int, center: Coord, size: ImageSize, rect: Rect
     ) -> None:
         self.classid = classid
         self.center = center
-        self.res = res
+        self.size = size
         self.rect = rect
 
     @classmethod
     def from_centerbox(
-        cls, center: Tuple[Number, Number], res: Tuple[Number, Number]
+        cls, classid: int, center: Tuple[float, float], size: Tuple[float, float]
     ):
-        pass
+        center_ = Coord(*center)
+        size_ = ImageSize(*size)
+
+        halfw = size_.width / 2.0
+        halfh = size_.height / 2.0
+        xmin = center_.x - halfw
+        xmax = center_.x + halfw
+        ymin = center_.y - halfh
+        ymax = center_.y + halfh
+        rect_ = Rect(Coord(xmin, ymin), Coord(xmax, ymax))
+
+        box = cls(classid, center_, size_, rect_)
+        return box
 
     @classmethod
-    def from_rect(cls, rect: Tuple[Number, Number, Number, Number]):
+    def from_rect(cls, rect: Tuple[float, float, float, float]):
         raise NotImplementedError('Not needed yet, so not implemented yet')
