fd240a2514963d956109a491f07f1b45165457be
DOHEE <dohee212@naver.com>
2022-06-16 16:35:34 +0900

    [Feat] 사용자 프로필 수정 기능 수정

--- back/src/routes/userRouter.js
+++ back/src/routes/userRouter.js
@@ -55,7 +55,7 @@ userRouter.get("/:userId/myPage", loginRequired, async (req, res, next) => {
     }
 });
 
-userRouter.put("/:userId/profile", loginRequired, async (req, res, next) => {
+userRouter.patch("/:userId/profile", loginRequired, async (req, res, next) => {
     try {
         // URI로부터 사용자 id를 추출함.
         const loginId = req.currentUserId;
