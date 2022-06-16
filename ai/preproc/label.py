2f99b49f13e31b22464ababa15af3602e8bc3540
YunSeongJun <movomo@gmail.com>
2022-06-16 17:08:05 +0900

    code: add class Label, Box

---
+++ ai/preproc/label.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+
+class Label(object):
+    '''객체 탐지 이미지 하나를 표현하는 라벨입니다.'''
+
+    def __init__(self) -> None:
+        pass
+
+
+class Box(object):
+    '''객체 탐지 시 바운딩 박스 한개입니다.'''
