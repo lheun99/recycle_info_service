f71ddcc62d0ea956ef657fd64b790be87dbc56af
Lee Ha-eun <lheun99@gmail.com>
2022-06-17 15:10:57 +0900

    feat: recycleInfo, post, imgUpload, search 테스트 데이터 추가

--- back/src/routes/imgUploadRouter.js
+++ back/src/routes/imgUploadRouter.js
@@ -7,8 +7,9 @@ imgUploadRouter.post(
   async (req, res, next) => {
     try {
       //array선언에 따른 전체 정보 다루는 방향으로 수정 필요
-      const imgUrl = req.files[0].location;
-
+      //const imgUrl = req.files[0].location;
+      const imgUrl =
+        "https://team9-cyberdyne.s3.ap-northeast-2.amazonaws.com/post_img/1655309077778_test-img-2.jpg";
       const result = { message: "success", data: imgUrl };
       res.status(201).json(result);
     } catch (error) {
--- back/src/services/postService.js
+++ back/src/services/postService.js
@@ -3,14 +3,107 @@ const Post = require("../models/funcs/Post");
 const postService = {
   createPost: async ({ userId, title, post_img, content }) => {
     const newPost = { user_id: userId, title, post_img, content };
-    const createdNewPost = await Post.create({ newPost });
-
+    // const createdNewPost = await Post.create({ newPost });
+    const createdNewPost = {
+      post_id: "21",
+      user_id: "6da56e43-f9b1-4e19-9e4a-58257b189c06",
+      title: "post를 test합니다.",
+      content: "post test를 위해 post를 작성합니다.",
+      updatedAt: "2022-06-15T16:02:27.239Z",
+      createdAt: "2022-06-15T16:02:27.239Z",
+      post_img: null,
+    };
     return { message: "success", data: createdNewPost };
   },
 
   getAllPost: async () => {
-    const listedPost = await Post.findAllPost();
-
+    //const listedPost = await Post.findAllPost();
+    const listedPost = [
+      {
+        title: "post를 수정하는 기능을 test합니다.",
+        createdAt: "2022-06-14T13:14:05.516Z",
+        nickname: "test",
+        post_img: null,
+      },
+      {
+        title: "post를 test합니다.",
+        createdAt: "2022-06-14T13:20:33.441Z",
+        nickname: "test2",
+        post_img: null,
+      },
+      {
+        title: "post를 test합니다.",
+        createdAt: "2022-06-14T13:21:57.641Z",
+        nickname: "test2",
+        post_img: null,
+      },
+      {
+        title: "post를 test합니다.",
+        createdAt: "2022-06-14T13:33:06.573Z",
+        nickname: "test2",
+        post_img: null,
+      },
+      {
+        title: "post를 test합니다.",
+        createdAt: "2022-06-14T13:35:45.420Z",
+        nickname: "test",
+        post_img: null,
+      },
+      {
+        title: "post를 test합니다.",
+        createdAt: "2022-06-14T13:35:46.351Z",
+        nickname: "test",
+        post_img: null,
+      },
+      {
+        title: "post를 test합니다.",
+        createdAt: "2022-06-14T13:35:47.104Z",
+        nickname: "test",
+        post_img: null,
+      },
+      {
+        title: "post를 test합니다.",
+        createdAt: "2022-06-14T13:35:47.612Z",
+        nickname: "test",
+        post_img: null,
+      },
+      {
+        title: "post를 test합니다.",
+        createdAt: "2022-06-14T13:35:48.140Z",
+        nickname: "test",
+        post_img: null,
+      },
+      {
+        title: "post를 test합니다.",
+        createdAt: "2022-06-15T15:01:12.393Z",
+        nickname: "lheun",
+        post_img: null,
+      },
+      {
+        title: "post를 test합니다.",
+        createdAt: "2022-06-15T15:25:01.316Z",
+        nickname: "lheun",
+        post_img: null,
+      },
+      {
+        title: "post를 수정하는 기능을 test합니다.",
+        createdAt: "2022-06-15T15:30:54.246Z",
+        nickname: "lheun",
+        post_img: null,
+      },
+      {
+        title: "post를 수정하는 기능을 test합니다.",
+        createdAt: "2022-06-15T15:59:37.313Z",
+        nickname: "lheun",
+        post_img: null,
+      },
+      {
+        title: "post를 test합니다.",
+        createdAt: "2022-06-15T16:02:27.239Z",
+        nickname: "lheun",
+        post_img: null,
+      },
+    ];
     if (!listedPost) {
       const errorMessage = "게시글 존재하지 않습니다.";
       return { errorMessage };
@@ -20,8 +113,54 @@ const postService = {
   },
 
   getPostById: async ({ userId }) => {
-    const currentPost = await Post.findPostByUserId({ user_id: userId });
-
+    //const currentPost = await Post.findPostByUserId({ user_id: userId });
+    const currentPost = [
+      {
+        post_id: "14",
+        title: "post를 test합니다.",
+        post_img: null,
+        content: "post test를 위해 post를 작성합니다.",
+        user_id: "6da56e43-f9b1-4e19-9e4a-58257b189c06",
+        createdAt: "2022-06-15T15:01:12.393Z",
+        updatedAt: "2022-06-15T15:01:12.393Z",
+      },
+      {
+        post_id: "15",
+        title: "post를 test합니다.",
+        post_img: null,
+        content: "post test를 위해 post를 작성합니다.",
+        user_id: "6da56e43-f9b1-4e19-9e4a-58257b189c06",
+        createdAt: "2022-06-15T15:25:01.316Z",
+        updatedAt: "2022-06-15T15:25:01.316Z",
+      },
+      {
+        post_id: "16",
+        title: "post를 수정하는 기능을 test합니다.",
+        post_img: null,
+        content: "post test를 위해 첫번째 post를 수정합니다.",
+        user_id: "6da56e43-f9b1-4e19-9e4a-58257b189c06",
+        createdAt: "2022-06-15T15:30:54.246Z",
+        updatedAt: "2022-06-15T15:42:51.109Z",
+      },
+      {
+        post_id: "19",
+        title: "post를 수정하는 기능을 test합니다.",
+        post_img: null,
+        content: "post test를 위해 첫번째 post를 수정합니다.",
+        user_id: "6da56e43-f9b1-4e19-9e4a-58257b189c06",
+        createdAt: "2022-06-15T15:59:37.313Z",
+        updatedAt: "2022-06-15T16:01:51.163Z",
+      },
+      {
+        post_id: "21",
+        title: "post를 test합니다.",
+        post_img: null,
+        content: "post test를 위해 post를 작성합니다.",
+        user_id: "6da56e43-f9b1-4e19-9e4a-58257b189c06",
+        createdAt: "2022-06-15T16:02:27.239Z",
+        updatedAt: "2022-06-15T16:02:27.239Z",
+      },
+    ];
     if (!currentPost) {
       const errorMessage =
         "게시글 존재하지 않습니다. 다시 한 번 확인해 주세요.";
@@ -31,8 +170,16 @@ const postService = {
     return { message: "success", data: currentPost };
   },
   getPost: async ({ post_id }) => {
-    const currentPost = await Post.findPostById({ post_id });
-
+    //const currentPost = await Post.findPostById({ post_id });
+    const currentPost = {
+      post_id: "19",
+      title: "post를 수정하는 기능을 test합니다.",
+      post_img: null,
+      content: "post test를 위해 첫번째 post를 수정합니다.",
+      user_id: "6da56e43-f9b1-4e19-9e4a-58257b189c06",
+      createdAt: "2022-06-15T15:59:37.313Z",
+      updatedAt: "2022-06-15T16:01:51.163Z",
+    };
     if (!currentPost) {
       const errorMessage =
         "게시글 존재하지 않습니다. 다시 한 번 확인해 주세요.";
@@ -43,8 +190,16 @@ const postService = {
   },
 
   setPost: async ({ post_id, toUpdate }) => {
-    const findedPost = await Post.findPostById({ post_id });
-
+    //const findedPost = await Post.findPostById({ post_id });
+    const findedPost = {
+      post_id: "19",
+      title: "post를 수정하는 기능을 test합니다.",
+      post_img: null,
+      content: "post test를 위해 첫번째 post를 수정합니다.",
+      user_id: "6da56e43-f9b1-4e19-9e4a-58257b189c06",
+      createdAt: "2022-06-15T15:59:37.313Z",
+      updatedAt: "2022-06-15T16:01:51.163Z",
+    };
     if (!findedPost) {
       const errorMessage =
         "게시글 존재하지 않습니다. 다시 한 번 확인해 주세요.";
@@ -62,10 +217,10 @@ const postService = {
   },
 
   deletePost: async ({ post_id }) => {
-    const deletedPost = await Post.delete({
-      post_id,
-    });
-
+    // const deletedPost = await Post.delete({
+    //   post_id,
+    // });
+    const deletedPost = 1;
     if (!deletedPost) {
       const errorMessage =
         "게시글 존재하지 않습니다. 다시 한 번 확인해 주세요.";
