9acc2db9cfef7705a39091052f9b397c46450155
YunSeongJun <movomo@gmail.com>
2022-06-19 19:18:39 +0900

    chore: adjust args for resuming

--- ai/train/train.sh
+++ ai/train/train.sh
@@ -5,18 +5,19 @@ YOLOREPO="$HOME/yolov5"
 DST="$HOME/train"
 
 
-# --img-size 320, 4에포크 시간 측정
+# epoch 0:8 DONE
+#     --cfg "$YOLOREPO/models/yolov5s.yaml" \
+# epoch 8:14 ...
 python "$YOLOREPO/train.py" \
-    --weights '' \
-    --cfg "$YOLOREPO/models/yolov5s.yaml" \
+    --weights "$DST/main/main.0/weights/last.pt" \
     --data "$REPO/ai/train/data-train.yaml" \
     --hyp "$REPO/ai/train/hyp.init.yaml" \
     --img-size 320 \
     --batch-size -1 \
     --project "$DST/main" \
-    --name 'main.0' \
+    --name 'main.8' \
     --optimizer 'AdamW' \
     --image-weights \
-    --patience 10 \
-    --epochs 8 \
+    --patience 4 \
+    --epochs 6 \
     --workers 3
