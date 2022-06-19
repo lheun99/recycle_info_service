b272641739d00cbc75e6d6dda97608b5b44764ef
YunSeongJun <movomo@gmail.com>
2022-06-19 17:11:18 +0900

    feat: add multiprocessing

--- ai/preproc/test-image.py
+++ ai/preproc/test-image.py
@@ -1,22 +1,25 @@
 #! /usr/bin/env python3
 
 import argparse
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import cpu_count
 from os import path
 import os
+from typing import Tuple
 
 from PIL import Image
 from tqdm import tqdm
 
 
-def valid_image(pathname: str) -> bool:
+def valid_image(pathname: str) -> Tuple[bool, str]:
     '''이미지가 제대로 된 파일인지 검사하고 정상이면 참, 아니면 거짓을 반환합니다.'''
     try:
         with Image.open(pathname) as image:
             image.verify()
     except Exception:
-        return False
+        return (False, pathname)
     else:
-        return True
+        return (True, pathname)
 
 
 def cli():
@@ -36,9 +39,24 @@ def cli():
         for leaf in leaves
     ]
 
-    for task in tqdm(tasks, desc='Verifying images'):
-        if not valid_image(task):
-            print(path.relpath(args.src, task))
+    executor = ProcessPoolExecutor()
+    with tqdm(
+        total=len(tasks),
+        desc=f'Verifying images ({executor._max_workers} workers)',
+    ) as pbar:
+        for valid, pathname in executor.map(
+            valid_image,
+            tasks,
+            chunksize=max(1, round(len(tasks) / 100 / cpu_count())),
+        ):
+            if not valid:
+                print(path.relpath(args.src, pathname))
+            pbar.update(1)
+    executor.shutdown()
+
+    # for task in tqdm(tasks, desc='Verifying images'):
+    #     if not valid_image(task)[0]:
+    #         print(path.relpath(args.src, task))
 
 
 if __name__ == '__main__':
