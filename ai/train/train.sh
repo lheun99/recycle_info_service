57d2a72629357de650a5eb3b2d41a205c700b5bb
YunSeongJun <movomo@gmail.com>
2022-06-20 13:57:36 +0900

    chore: change args for resume

--- ai/train/train.sh
+++ ai/train/train.sh
@@ -7,17 +7,18 @@ DST="$HOME/train"
 
 # epoch 0:8 DONE
 #     --cfg "$YOLOREPO/models/yolov5s.yaml" \
-# epoch 8:14 ...
+# epoch 8:14 DONE
+# epoch 14: ...
 python "$YOLOREPO/train.py" \
-    --weights "$DST/main/main.0/weights/last.pt" \
+    --weights "$DST/main/main.8/weights/last.pt" \
     --data "$REPO/ai/train/data-train.yaml" \
     --hyp "$REPO/ai/train/hyp.init.yaml" \
     --img-size 320 \
     --batch-size -1 \
     --project "$DST/main" \
-    --name 'main.8' \
+    --name 'main.14' \
     --optimizer 'AdamW' \
     --image-weights \
     --patience 4 \
-    --epochs 6 \
+    --epochs 300 \
     --workers 3
