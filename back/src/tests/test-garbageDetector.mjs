e44306bd50d477c2f427e61d0c15734bcc187ceb
YunSeongJun <movomo@gmail.com>
2022-06-21 16:48:26 +0900

    test: helpful info message

--- back/src/tests/test-garbageDetector.mjs
+++ back/src/tests/test-garbageDetector.mjs
@@ -4,6 +4,7 @@ import { AppError } from "../utils/errors.js";
 import { GarbageDetector } from "../utils/od/garbageDetector.mjs";
 
 // 1. 말도 안되는 경로를 집어넣고 에러를 제대로 내는지 봅니다.
+console.info(`Testing sanity check functionality\n\n`);
 try {
   const gd = new GarbageDetector(`ham/jam/spam.eggs`);
   console.warn(`FAIL: didn't error out`);
