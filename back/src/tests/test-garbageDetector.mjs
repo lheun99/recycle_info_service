7548fd1951a253588a25060c9eac954a3dc46547
YunSeongJun <movomo@gmail.com>
2022-06-21 18:12:11 +0900

    refactor: output format

--- back/src/tests/test-garbageDetector.mjs
+++ back/src/tests/test-garbageDetector.mjs
@@ -9,7 +9,7 @@ const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 /** 테스트를 수행하고 성공 여부를 객체로 반환합니다. */
 const main = async () => {
-  const result = [];
+  const result = {};
 
   // 1. 말도 안되는 경로를 집어넣고 에러를 제대로 내는지 봅니다.
   console.info(`Testing sanity check functionality\n\n`);
@@ -19,23 +19,33 @@ const main = async () => {
   } catch (error) {
     if (error instanceof AppError) {
       console.info(`Don't panic, everything is under control`);
+      result[`Sanity check test`] = true;
+    } else {
+      result[`Sanity check test`] = false;
     }
     console.info(error);
   }
   console.log(`\n\n`);
 
   // 2. 모델을 만듭니다.
-  const modelPath = path.resolve(
-    __dirname,
-    `..`,
-    //   `utils/od/gdmodel/weights/last_web_model/model.json`,
-    `utils/od/gdmodel/weights/last_saved_model`
-  );
-  console.info(`model's absolute path is "${modelPath}"`);
-  const gd = new GarbageDetector(modelPath);
-  await gd.init();
-  console.info(gd);
-  console.log(`\n\n`);
+  let gd;
+  try {
+    const modelPath = path.resolve(
+      __dirname,
+      `..`,
+      //   `utils/od/gdmodel/weights/last_web_model/model.json`,
+      `utils/od/gdmodel/weights/last_saved_model`
+    );
+    console.info(`model's absolute path is "${modelPath}"`);
+    gd = new GarbageDetector(modelPath);
+    await gd.init();
+    console.info(gd);
+    console.log(`\n\n`);
+    result[`Model loading test`] = true;
+  } catch (error) {
+    result[`Model loading test`] = false;
+    return result;
+  }
 };
 
-main().then(() => console.info(`DONE`));
+main().then((res) => console.info(res));
