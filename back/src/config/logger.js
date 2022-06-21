91121081c3fa1ae0df0dbd57aec83d94408f83c0
DOHEE <dohee212@naver.com>
2022-06-21 13:37:00 +0900

    [Feat] level 설정

--- back/src/config/logger.js
+++ back/src/config/logger.js
@@ -1,3 +1,11 @@
 const winston = require("winston");
 require("winston-daily-rotate-file");
 const logDir = `${__dirname}/log`;
+
+const levels = { error: 0, warn: 1, info: 2, http: 3, debug: 4 };
+
+const level = () => {
+    const env = process.env.NODE_ENV || "development";
+    const isDevelopment = env === "development";
+    return isDevelopment ? "debug" : "warn";
+};
