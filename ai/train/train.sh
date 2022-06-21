dc3dbd35ce5ee8a5ab2a26767bf7d71ca6ffaa6c
YunSeongJun <movomo@gmail.com>
2022-06-22 00:32:57 +0900

    chore: change args for main.final train

--- ai/train/train.sh
+++ ai/train/train.sh
@@ -8,18 +8,19 @@ DST="$HOME/train"
 # epoch 0:8 DONE
 #     --cfg "$YOLOREPO/models/yolov5s.yaml" \
 # epoch 8:14 DONE
-# epoch 14: ...
+# epoch 14:19 DONE
+# epoch 19: ...
 python "$YOLOREPO/train.py" \
-    --weights "$DST/main/main.8/weights/last.pt" \
+    --weights "$DST/main/main.14/weights/last.pt" \
     --data "$REPO/ai/train/data-train.yaml" \
     --hyp "$REPO/ai/train/hyp.init.yaml" \
     --img-size 320 \
     --batch-size -1 \
     --project "$DST/main" \
-    --name 'main.14' \
+    --name 'main.final' \
     --optimizer 'AdamW' \
     --image-weights \
     --patience 4 \
-    --epochs 300 \
+    --epochs 90 \
     --workers 3 \
     --save-period 1
