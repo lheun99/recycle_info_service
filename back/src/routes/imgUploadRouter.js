fbf029f084eafd1ecfc42bf6485c520218f56ea7
Lee Ha-eun <lheun99@gmail.com>
2022-06-21 17:26:44 +0900

    feat: profile-img-upload 추가

--- back/src/routes/imgUploadRouter.js
+++ back/src/routes/imgUploadRouter.js
@@ -1,14 +1,31 @@
 const imgUploadRouter = require("express").Router();
-const upload = require("../middlewares/imgUploader.js");
+const {
+  uploadPostImage,
+  uploadProfileImage,
+} = require("../middlewares/imgUploader.js");
 
 imgUploadRouter.post(
   "/post-img",
-  upload.array("image"),
+  uploadPostImage.array("image"),
   async (req, res, next) => {
     try {
       const files = req.files;
-      const imgUrl = [];
-      files.map((file) => imgUrl.push(file.location));
+      const imgUrl = files.map((file) => file.location);
+
+      const result = { message: "success", data: imgUrl };
+      res.status(201).json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+imgUploadRouter.post(
+  "/profile-img",
+  uploadProfileImage.single("image"),
+  async (req, res, next) => {
+    try {
+      const imgUrl = req.file.location;
 
       const result = { message: "success", data: imgUrl };
       res.status(201).json(result);
