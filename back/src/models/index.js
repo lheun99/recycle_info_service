27ae775cd193a02262d46a761d9fa18bece0c262
DOHEE <dohee212@naver.com>
2022-06-21 10:51:12 +0900

    [Feat] git pull dev-back

--- back/src/models/index.js
+++ back/src/models/index.js
@@ -9,33 +9,35 @@ const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
 let sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
-  port: config.port,
-  dialect: "postgres",
-  logging: false,
+    username: config.username,
+    password: config.password,
+    database: config.database,
+    host: config.host,
+    port: config.port,
+    dialect: "postgres",
+    logging: false,
 });
 
 fs.readdirSync(__dirname)
-  .filter((file) => {
-    return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-    );
-  })
-  .forEach((file) => {
-    const model = require(path.join(__dirname, file))(
-      sequelize,
-      Sequelize.DataTypes
-    );
-    db[model.name] = model;
-  });
+    .filter((file) => {
+        return (
+            file.indexOf(".") !== 0 &&
+            file !== basename &&
+            file.slice(-3) === ".js"
+        );
+    })
+    .forEach((file) => {
+        const model = require(path.join(__dirname, file))(
+            sequelize,
+            Sequelize.DataTypes
+        );
+        db[model.name] = model;
+    });
 
 Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
 });
 
 db.sequelize = sequelize;
@@ -46,10 +48,10 @@ db.point = require("./schemas/points.js")(sequelize, Sequelize);
 db.quiz = require("./schemas/quizs.js")(sequelize, Sequelize);
 //model정의
 db.post = require("./schemas/posts.js")(sequelize, Sequelize);
-db.recycleInfo = require("./schemas/recycle_infos.js")(sequelize, Sequelize);
 db.recycleCategory = require("./schemas/recycle_categories.js")(
-  sequelize,
-  Sequelize
+    sequelize,
+    Sequelize
 );
+db.recycleInfo = require("./schemas/recycle_infos.js")(sequelize, Sequelize);
 
 module.exports = db;
