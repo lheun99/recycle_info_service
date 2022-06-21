ea4d69a864e7e703b092b9cefe9e9e9905bd4b2c
DOHEE <dohee212@naver.com>
2022-06-21 13:41:03 +0900

    [Feat] format 설정

--- back/src/config/logger.js
+++ back/src/config/logger.js
@@ -19,3 +19,11 @@ const colors = {
 };
 
 winston.addColors(colors);
+
+const format = winston.format.combine(
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:MM:SS ||" }),
+    winston.format.colorize({ all: true }),
+    winston.format.printf(
+        (info) => `${info.timestamp} [ ${info.level} ] ▶ ${info.message}`
+    )
+);
