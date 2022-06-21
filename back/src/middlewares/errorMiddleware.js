b2d8d66296d451abf01e24c81ab0ee1b6b83f75f
DOHEE <dohee212@naver.com>
2022-06-21 13:51:55 +0900

    [Feat] logger 설정

--- back/src/middlewares/errorMiddleware.js
+++ back/src/middlewares/errorMiddleware.js
@@ -1,6 +1,9 @@
+const logger = require("../config/logger");
+
 const errorMiddleware = (error, req, res, next) => {
     // 터미널에 노란색으로 출력됨.
     console.log("\x1b[33m%s\x1b[0m", error);
+    logger.error(error);
     res.status(400).json({ message: "fail", data: error.message });
 };
 
