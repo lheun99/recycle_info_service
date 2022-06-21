29a3dc81003503e1511f114ae7d7664a72a8a42c
YunSeongJun <movomo@gmail.com>
2022-06-21 13:13:07 +0900

    code: arg adjustment for sgd.evolve testing

--- ai/train/exp-sgd.sh
+++ ai/train/exp-sgd.sh
@@ -5,19 +5,15 @@ YOLOREPO="$HOME/yolov5"
 DST="$HOME/train"
 
 
-# epoch 0:8 DONE
-#     --cfg "$YOLOREPO/models/yolov5s.yaml" \
-# epoch 8:14 DONE
-# epoch 14: ...
 python "$YOLOREPO/train.py" \
     --weights '' \
     --cfg "$YOLOREPO/models/yolov5s.yaml" \
     --data "$REPO/ai/train/data-train.yaml" \
-    --hyp "$REPO/ai/train/hyp.sgd.yaml" \
+    --hyp "$REPO/ai/train/hyp.sgd.evolve.yaml" \
     --img-size 320 \
     --batch-size -1 \
     --project "$DST/main" \
-    --name 'exp.sgd' \
+    --name 'exp.sgd.evolve' \
     --optimizer 'SGD' \
     --image-weights \
     --patience 4 \
