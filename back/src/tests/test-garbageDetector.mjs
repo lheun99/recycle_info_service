e3be326810f6ebf3dd7e5e5ffe34843632b74292
YunSeongJun <movomo@gmail.com>
2022-06-22 15:49:08 +0900

    test: add perfhooks

--- back/src/tests/test-garbageDetector.mjs
+++ back/src/tests/test-garbageDetector.mjs
@@ -1,12 +1,17 @@
 import assert from "assert/strict";
 import * as fsp from "fs/promises";
 import path from "path";
+import { performance, PerformanceObserver } from "perf_hooks";
 import url from "url";
 
 import { AppError } from "../utils/errors.js";
 import { GarbageDetector } from "../utils/od/garbageDetector.mjs";
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const obs = new PerformanceObserver((items) => {
+  items.getEntries().forEach((entry) => console.log(entry));
+});
+obs.observe({ entryTypes: "measure" });
 
 /** 테스트를 수행하고 성공 여부를 객체로 반환합니다. */
 const main = async () => {
@@ -14,6 +19,7 @@ const main = async () => {
 
   // 1. 말도 안되는 경로를 집어넣고 에러를 제대로 내는지 봅니다.
   console.info(`** Testing bailout functionality **\n`);
+  performance.mark(`1.start`);
   try {
     assert.throws(
       () => {
@@ -27,10 +33,13 @@ const main = async () => {
     result[`Bailout test`] = false;
     console.info(error);
   }
+  performance.mark(`1.end`);
+  performance.measure(`1`, `1.start`, `1.end`);
   console.log(`\n`);
 
   // 2. 모델을 만듭니다.
   console.info(`** Trying to load a valid model **\n`);
+  performance.mark(`2.start`);
   let detector;
   try {
     const modelPath = path.resolve(
@@ -43,36 +52,46 @@ const main = async () => {
     detector = new GarbageDetector(modelPath);
     await detector.init();
     console.info(detector);
-    console.log(`\n`);
     result[`Model loading test`] = true;
   } catch (error) {
     result[`Model loading test`] = false;
     return result;
+  } finally {
+    performance.mark(`2.end`);
+    performance.measure(`2`, `2.start`, `2.end`);
   }
+  console.log(`\n`);
 
   // 이미지를 하나 넣어 봅니다.
-  console.info(`** Inference i/o format test ** \n`);
+  console.info(`** Inference i/o format test **\n`);
+  performance.mark(`3.start`);
   const image = await fsp.readFile(
     path.join(__dirname, `test-images`, `11-bicycle.jpg`)
   );
   const res = await detector.guess(image);
   console.info(res);
-  console.log(
-    res[0].classId,
-    res[0].imageWidth,
-    res[0].imageHeight,
-    res[0].name("en"),
-    res[0].name("ko"),
-    res[0]._xyxy.length
-  );
-  assert(
-    res[0].classId === 11 &&
-      res[0].imageWidth === 320 &&
-      res[0].imageHeight === 320 &&
-      res[0].name("en") === "bicycle" &&
-      res[0].name("ko") === "자전거" &&
-      res[0]._xyxy.length === 4
-  );
+  try {
+    assert(
+      res[0].classId === 11 &&
+        res[0].imageWidth === 320 &&
+        res[0].imageHeight === 320 &&
+        res[0].name("en") === "bicycle" &&
+        res[0].name("ko") === "자전거" &&
+        res[0]._xyxy.length === 4
+    );
+    result[`Inference io format test`] = true;
+  } catch (error) {
+    result[`Inference io format test`] = false;
+  }
+  performance.mark(`3.end`);
+  performance.measure(`3`, `3.start`, `3.end`);
+  console.log(`\n`);
+
+  // 이미지를 많이 넣어 봅니다!
+  console.info(`** Batch inference time measure **\n`);
+  performance.mark(`4.start`);
+  performance.mark(`4.end`);
+  performance.measure(`4`, `4.start`, `4.end`);
 
   return result;
 };
