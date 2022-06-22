4d93a590ed0a12988a3c9278588d3cec815b4ab1
YunSeongJun <movomo@gmail.com>
2022-06-22 14:06:52 +0900

    fix: incorrect default values of operational

--- back/src/utils/errors.js
+++ back/src/utils/errors.js
@@ -2,10 +2,11 @@ const {
   STATUS_400_BADREQUEST,
   STATUS_500_INTERNALSERVERERROR,
 } = require("./status.js");
+
 /** 기본 `Error` 타입에 더 구체적인 정보를 담을 수 있도록 확장한 범용 에러입니다. */
 class AppError extends Error {
   static status = STATUS_500_INTERNALSERVERERROR;
-  static operational = true;
+  static operational = false;
   static logas = "warn";
   /** 더 구체적인 정보를 담을 수 있도록 확장한 범용 에러입니다.
    *
@@ -18,7 +19,7 @@ class AppError extends Error {
    * }} kwargs - 일부는 생략 가능합니다. 전부 생략하려면 `{}`를 줍니다.
    *    - `name`: 기본값 `this.constructor.name` = `"AppError"`
    *    - `status`: http status code입니다. 기본값 `500 Internal Server Error`
-   *    - `operational`: 참이면 프로세스를 끝냅니다. 기본값 `true`
+   *    - `operational`: 거짓이면 프로세스를 끝냅니다. 기본값 `false`
    *    - `logas`: 거짓값이 아니면 해당 메소드를 사용해 로그합니다. 기본값 `warn`
    *    - `detail`: 에러 분류를 돕기 위한 추가 정보입니다.
    * @arg {string} [message] - `Error` 컨스트럭터로 릴레이할 메시지입니다.
@@ -44,20 +45,20 @@ class AppError extends Error {
 /** `AppError` 타입을 Bad Request 용으로 쓰기 편하게 확장한 에러입니다. */
 class RequestError extends AppError {
   static status = STATUS_400_BADREQUEST;
-  static operational = false;
+  static operational = true;
   static logas = "info";
   /** `AppError` 타입을 Bad Request 용으로 쓰기 편하게 확장한 에러입니다.
    *
    * @arg {{
    *  name?: string,
    *  status?: number,
-   *  exit?: number,
+   *  operational?: boolean,
    *  logas?: string,
    *  detail?: any
    * }} kwargs - 일부는 생략 가능합니다. 전부 생략하려면 `{}`를 줍니다.
    *    - `name`: 기본값 `this.constructor.name` = `"RequestError"`
    *    - `status`: http status code입니다. 기본값 `400 Bad Request`
-   *    - `operational`: 참이면 프로세스를 끝냅니다. 기본값 `false`
+   *    - `operational`: 거짓이면 프로세스를 끝냅니다. 기본값 `true`
    *    - `logas`: 거짓값이 아니면 해당 메소드를 사용해 로그합니다. 기본값 `info`
    *    - `detail`: 에러 분류를 돕기 위한 추가 정보입니다.
    * @arg {string} [message] - `Error` 컨스트럭터로 릴레이할 메시지입니다.
