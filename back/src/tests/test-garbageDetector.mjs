2b8e07620a3700d053246128823ab4d15517a2d8
YunSeongJun <movomo@gmail.com>
2022-06-23 19:28:46 +0900

    test: different toString arg

--- back/src/tests/test-garbageDetector.mjs
+++ back/src/tests/test-garbageDetector.mjs
@@ -79,7 +79,11 @@ const main = async () => {
   }
   const image = await fsp.readFile(imagePath);
   const res = await detector.guess(image);
-  console.info(res);
+  console.info(`short version:`, res);
+  console.info(
+    `long version:`,
+    res.map((v) => v.toString(true))
+  );
   try {
     assert(
       res[0].classId === 11 &&
