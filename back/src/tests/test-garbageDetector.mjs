f0246a390d1332cf4c65a72884064f6e9d26614c
YunSeongJun <movomo@gmail.com>
2022-06-23 19:29:33 +0900

    test: no buffered perf observer

--- back/src/tests/test-garbageDetector.mjs
+++ back/src/tests/test-garbageDetector.mjs
@@ -12,7 +12,7 @@ const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 const obs = new PerformanceObserver((items) => {
   items.getEntries().forEach((entry) => console.log(entry));
 });
-obs.observe({ entryTypes: ["measure"], buffer: true });
+obs.observe({ entryTypes: ["measure"], buffer: false });
 
 /** 테스트를 수행하고 성공 여부를 객체로 반환합니다. */
 const main = async () => {
