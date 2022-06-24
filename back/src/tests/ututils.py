1821ad21f732f946550b2fb3e9f218031179799d
YunSeongJun <movomo@gmail.com>
2022-06-24 20:22:02 +0900

    code: add login to setup

--- back/src/tests/ututils.py
+++ back/src/tests/ututils.py
@@ -32,12 +32,15 @@ class AuthMixin(object):
     userId: str
     token: str
     headers: str
+    registered: bool = False
+    loggedin: bool = False
 
     def setUp(self) -> None:
         self.connection = HTTPConnection(
             self.config['host'], self.config['port']
         )
 
+        # 회원 가입을 시도합니다.
         authdata = self.config['auth']
         body = json.dumps(
             {
@@ -54,20 +57,43 @@ class AuthMixin(object):
             headers=self.headers
         )
         res = self.connection.getresponse()
-        if res.status != HTTPStatus.CREATED:
-            raise Exception('Registration failed')
+        self.assertEqual(
+            res.status, HTTPStatus.CREATED, 'Registration failed'
+        )
+        self.registered = True
 
         data = json.loads(res.read())['data']
         self.nickname = data['nickname']
         self.email = data['email']
         self.userId = data['userId']
-
         self.headers = json.dumps(
             {
                 'Content-Type': 'application/json',
                 'Authorization': f'Bearer {self.token}'
             }
         )
+
+        # 로그인을 시도합니다.
+        body = json.dumps(
+            {
+                'email': self.email,
+                'password': self.password
+            }
+        )
+        self.connection.request(
+            'POST',
+            f'{self.root}/users/login',
+            body=body,
+            headers=self.headers
+        )
+
+        res = self.connection.getresponse()
+        self.assertEqual(
+            res.status, HTTPStatus.OK, 'Login failed'
+        )
+        self.loggedin = True
+        data = json.loads(res.read())['data']
+        self.token = data['token']
 
     def tearDown(self) -> None:
         self.connection.request(
