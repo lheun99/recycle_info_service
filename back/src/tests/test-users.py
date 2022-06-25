257a45efefd92709089120c455c65ea93bbd9d67
YunSeongJun <movomo@gmail.com>
2022-06-25 17:14:48 +0900

    code: implement method _update_password

--- back/src/tests/test-users.py
+++ back/src/tests/test-users.py
@@ -8,6 +8,9 @@ from ututils import ConfigMixin, Identity
 
 
 class TestUsers(unittest.TestCase, ConfigMixin):
+    """``users`` api를 검사합니다."""
+    name = 'users'
+
     def _update_profile(self, imposter: Identity):
         conn = imposter.connection
         cfg = self.config['update_profile']
@@ -22,8 +25,23 @@ class TestUsers(unittest.TestCase, ConfigMixin):
         self.assertEqual(res.status, HTTPStatus.OK)
 
     def _update_password(self, imposter: Identity):
-        conn = self.myself.connection
+        conn = imposter.connection
         cfg = self.config['update_password']
+
+        password = cfg['body']['password']
+        conn.request(
+            'PATCH',
+            f'{self.root}/{cfg["path"]}'.format(**vars(self.myself)),
+            body=json.dumps(cfg['body']),
+            headers=imposter.headers
+        )
+        res = conn.getresponse()
+        self.assertEqual(res.status, HTTPStatus.OK)
+
+        # 비밀번호 변경에 성공하면 로그인을 해봅니다.
+        # 로그인에 실패하면 AssertionError가 전파됩니다.
+        self.myself.password = password
+        self.myself.login()
 
     def test_illegal_modification(self):
         conn = self.connection
