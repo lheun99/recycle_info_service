8ec51b32c159a5777a2a123918e0e2315121c97c
YunSeongJun <movomo@gmail.com>
2022-06-25 16:55:00 +0900

    refactor: generate_password to passwd

--- back/src/tests/ututils.py
+++ back/src/tests/ututils.py
@@ -77,7 +77,7 @@ class Identity(object):
             self.email = email
 
         if password is None:
-            self.password = self.generate_password()
+            self.password = self.passwd()
         else:
             self.nickname = nickname
 
@@ -143,7 +143,7 @@ class Identity(object):
         self.connection.close()
 
     @staticmethod
-    def generate_password():
+    def passwd():
         return ''.join(
             secrets.choice(__class__.pw_chars)
             for _ in range(random.randint(__class__.pw_len))
