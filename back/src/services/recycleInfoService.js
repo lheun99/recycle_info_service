5c10ea872e49133a503a9819a2d9a437b7e42f4f
Lee Ha-eun <lheun99@gmail.com>
2022-06-25 18:34:05 +0900

    fix: recycleInfo 전달 데이터 형태 수정 진행 중

--- back/src/services/recycleInfoService.js
+++ back/src/services/recycleInfoService.js
@@ -4,27 +4,44 @@ const recycleInfoService = {
   //POST /recycle-info
   analysisImg: async ({ imgBuffer }) => {
     //인공지능 파트로 이미지 정보 전달, 분석 결과
-    const result = await RecycleInfo.findRecycleCode({ imgBuffer });
-    if (result.length === 0) {
-      const errorMessage = "분석 실패! 재촬영 요청";
-      return { errorMessage };
+    const results = await RecycleInfo.findRecycleCode({ imgBuffer });
+
+    if (results.length === 0) {
+      return { message: "success", data: { typeCount: 0, infos: [] } };
     }
-    //분석 결과 중 recycle_category_code 추출
-    const code = result[0].classId;
+
+    const analysisImgResult = results.map((result) => ({
+      code: result.classId,
+      confidence: (result.confidence * 100).toFixed(2),
+      xyxy: result._xyxy,
+    }));
 
     //recycle_category_code에 따른 분리배출 정보
-    const infos = await RecycleInfo.findInfoByCode({ code });
-
-    //안내될 정보 페이지 수
-    const page = infos.length;
     //전달 데이터 형태 변경
-    const category = infos[0].category;
-    const recycleInfo = [];
-    infos.map((info) =>
-      recycleInfo.push({ details: info.details, infoImg: info.info_img })
+    const infos = await Promise.all(
+      analysisImgResult.map(async (result) => {
+        const info = await RecycleInfo.findInfoByCode({ code: result.code });
+
+        const category = info[0].category;
+        result.category = category;
+
+        const infoPageCount = info.length;
+        result.infoPageCount = infoPageCount;
+
+        const recycleInfo = info.map((info) => ({
+          details: info.details,
+          infoImg: info.info_img,
+        }));
+        result.recycleInfo = recycleInfo;
+
+        return result;
+      })
     );
 
-    return { message: "success", data: { page, category, recycleInfo } };
+    //안내될 정보 페이지 수
+    const typeCount = infos.length;
+
+    return { message: "success", data: { typeCount, infos } };
   },
 
   //GET /recycle-info/?code
@@ -42,10 +59,10 @@ const recycleInfoService = {
     const page = infos.length;
     //전달 데이터 형태 변경
     const category = infos[0].category;
-    const recycleInfo = [];
-    infos.map((info) =>
-      recycleInfo.push({ details: info.details, infoImg: info.info_img })
-    );
+    const recycleInfo = infos.map((info) => ({
+      details: info.details,
+      infoImg: info.info_img,
+    }));
 
     return { message: "success", data: { page, category, recycleInfo } };
   },
