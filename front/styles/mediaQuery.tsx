f3fb39ccc3054b53991566e33695f8d38fa14787
jinah park <tbrk8585@naver.com>
2022-06-28 22:37:56 +0900

    feat: style usemediaQuery function

--- front/styles/mediaQuery.tsx
+++ front/styles/mediaQuery.tsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useMediaQuery } from "react-responsive";
 
+// 모바일 기기
 export function useIsMobile() {
     const [isMobile, setIsMobile] = useState(false);
-    const mobile = useMediaQuery({ query: "(max-width: 600px)" });
+    const mobile = useMediaQuery({ query: "(max-width: 600px)" }); // 600 이나 500 정도
     useEffect(() => {
         setIsMobile(mobile);
     }, [mobile]);
@@ -11,6 +12,24 @@ export function useIsMobile() {
     return isMobile;
 }
 
-// export  function isMiddle () {`
+// 패드 류, 미들
+export function useIsMiddle() {
+    const [isMiddle, setIsMiddle] = useState(false);
+    const Middle = useMediaQuery({ query: "(max-width: 900px)" });
+    useEffect(() => {
+        setIsMiddle(Middle);
+    }, [Middle]);
 
-// }
+    return isMiddle;
+}
+
+// 와이드
+export function useIsWide() {
+    const [isWide, setIsWide] = useState(false);
+    const Wide = useMediaQuery({ query: "(max-width: 1224px)" });
+    useEffect(() => {
+        setIsWide(Wide);
+    }, [Wide]);
+
+    return isWide;
+}
