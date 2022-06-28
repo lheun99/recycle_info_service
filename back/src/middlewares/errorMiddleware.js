777d161fea9c707cf840a145b61e0a0568ee42d5
DOHEE <dohee212@naver.com>
2022-06-28 15:19:38 +0900

    [Feat] errorMiddleware에서 필요없는 console.log 제거

--- back/src/middlewares/errorMiddleware.js
+++ back/src/middlewares/errorMiddleware.js
@@ -1,10 +1,8 @@
 import logger from "../config/logger.js";
 
 const errorMiddleware = (error, req, res, next) => {
-  logger.error(error);
-  // 터미널에 노란색으로 출력됨.
-  console.log("\x1b[33m%s\x1b[0m", error);
-  res.status(400).json({ message: "fail", data: error.message });
+    logger.error(error);
+    res.status(400).json({ message: "fail", data: error.message });
 };
 
 export default errorMiddleware;
