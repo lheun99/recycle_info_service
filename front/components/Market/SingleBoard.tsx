fbcf7a8a27dc92c6f31355d7f684532797f4daf3
jinah park <tbrk8585@naver.com>
2022-06-30 20:31:48 +0900

    feat: market user profile fix & recycle point post add

--- front/components/Market/SingleBoard.tsx
+++ front/components/Market/SingleBoard.tsx
@@ -1,5 +1,5 @@
-import React, { useContext, useState, useRef, useEffect } from "react";
-import { UserStateContext } from "../../pages/_app";
+import React, { useState, useRef, useEffect } from "react";
+
 import Comment from "./Comment";
 
 import styled from "styled-components";
@@ -24,7 +24,6 @@ import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
 import { useTheme } from "@mui/material/styles";
 import nextArrow from "../../public/images/next.arrow.png";
 import Image from "next/image";
-import { SignalCellularNullTwoTone } from "@mui/icons-material";
 
 interface ExpandMoreProps extends IconButtonProps {
     expand: boolean;
@@ -47,9 +46,7 @@ const SingleBoard = ({ item }) => {
     const [slideIndex, setSlideIndex] = useState(0);
     const [activeStep, setActiveStep] = useState(0);
     const maxSteps = item?.postImg?.length ?? 0; // 자료의 총 길이
-    const userInfo = useContext(UserStateContext);
     const viewContainerRef = useRef<HTMLDivElement>(null);
-    const profileImg = userInfo?.user?.picture ?? "";
     const theme = useTheme();
 
     const handleExpandClick = () => {
@@ -167,7 +164,7 @@ const SingleBoard = ({ item }) => {
                 />
 
                 <CardWriterContainer
-                    avatar={<Avatar alt="userProfile" src={profileImg} />}
+                    avatar={<Avatar alt="userProfile" src={item.userImg} />}
                     action={
                         <IconButton aria-label="settings">
                             <MoreVertIcon />
