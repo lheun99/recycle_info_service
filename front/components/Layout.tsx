cf2d0b52426a2353e3edb65c0b04ef2e3ff14860
jinah park <tbrk8585@naver.com>
2022-07-02 02:50:25 +0900

    fix: footer fixed

--- front/components/Layout.tsx
+++ front/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Nav from "./Nav";
 import Footer from "./Footer";
+import styled from "styled-components";
 
 type AppLayoutProps = {
     children: React.ReactNode;
@@ -11,10 +12,14 @@ const Layout = ({ children }: AppLayoutProps) => {
         <div>
             <title>구해줘!, 지구</title>
             <Nav />
-            <div>{children}</div>
+            <BodyWrapper>{children}</BodyWrapper>
             <Footer />
         </div>
     );
 };
 
 export default Layout;
+
+const BodyWrapper = styled.div`
+    min-height: 100vh;
+`;
