c64780a6644f51a1656b8874883cf5eafcaaa51e
jinah park <tbrk8585@naver.com>
2022-07-15 01:56:47 +0900

    chore: market none use import delete

--- front/pages/market.tsx
+++ front/pages/market.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "styled-components";
 import MainBoard from "../components/Market/MainBoard";
 import { getPost } from "../api";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
